016026049e71c4e73582b52f2bac5c9f1ac2dd3f
Ubuntu <ubuntu@ip-172-31-30-131.ap-northeast-1.compute.internal>
2016-03-12 07:55:37 +0000

    add commands for setup job

---
+++ commands.sh
@@ -0,0 +1,7 @@
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install python-pip git
+sudo pip install Django==1.9.4
+mkdir git && cd git
+git clone https://github.com/a180285/django-learn.git
+
