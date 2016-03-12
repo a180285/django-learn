bacef883e30c97b49c03d18789fda60bf4490e95
Ubuntu <ubuntu@ip-172-31-30-131.ap-northeast-1.compute.internal>
2016-03-12 08:17:33 +0000

    add zsh

--- commands.sh
+++ commands.sh
@@ -1,6 +1,12 @@
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install python-pip git
+sudo apt-get install python-pip git zsh wget
+
+# Install zsh
+wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
+echo "/bin/zsh" >> .bashrc
+
+# Install django
 sudo pip install Django==1.9.4
 mkdir git && cd git
 git clone https://github.com/a180285/django-learn.git
