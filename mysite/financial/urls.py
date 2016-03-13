4d995e477457d60127e834ced4378a608746c6d5
Wei Huang <huangwei.a180285@gmail.com>
2016-03-13 17:40:47 +0800

    add financial views

--- mysite/financial/urls.py
+++ mysite/financial/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'financial'
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^account/$', views.account, name='account'),
 ]
--- mysite/financial/views.py
+++ mysite/financial/views.py
@@ -3,6 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import get_object_or_404
+from django.template import loader
+from .models import Account
 
 def index(request):
-    return HttpResponse("Hello, world. This is financial first page")
+  return HttpResponse("Hello, world. This is financial first page")
+
+def account(request):
+  accounts = Account.objects.order_by("name")
+  context = {'account_list': accounts}
+  return render(request, 'financial/account.html', context)
