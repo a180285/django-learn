232d071680754b7a86d3ad6d496ae109aa1f565e
Wei Huang <huangwei.a180285@gmail.com>
2016-04-01 23:12:47 +0800

    add commands

--- django_commands.sh
+++ django_commands.sh
@@ -2,6 +2,8 @@ django-admin startproject mysite
 python manage.py runserver
 python manage.py runserver 0.0.0.0:8000
 sudo python manage.py runserver 0.0.0.0:80
+nohup python manage.py runserver 127.0.0.1:8000 2>&1 1>log &
+./manage.py collectstatic -v0 --noinput
 
 python manage.py startapp polls
 
