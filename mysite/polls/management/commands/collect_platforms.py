c335292b7063b3383b2adbb8b65ca859245a691f
Wei Huang <huangwei.a180285@gmail.com>
2016-04-11 22:31:17 +0800

    test

--- mysite/polls/management/commands/collect_platforms.py
+++ mysite/polls/management/commands/collect_platforms.py
@@ -28,7 +28,7 @@ class Command(BaseCommand):
     ]
 
     # TODO: Show hint for interest_manage_rate
-    platforms = [YiQiHao()]
+    # platforms = [YiQiHao()]
 
     for p in platforms:
       try:
